4efe2fce037cdace6dd1257e539a2f7b0da56c17
Paige Jones <hello@paigejones.me>
2021-10-21 13:27:39 +0100

    fix vuelidate by switching from plugin to component

---
+++ vuelidate/component.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<input v-model="form.email_address" />
+		<div v-for="error of validate.form.email_address.$errors" :key="error.$uid">
+			<div class="error-msg">
+				{{ error.$message }}
+			</div>
+		</div>
+
+		<button @click="submit">Submit</button>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { ref, Ref } from 'vue'
+import useVuelidate from '@vuelidate/core'
+import { required, email } from '@vuelidate/validators'
+
+const form = ref({
+	email_address: '' as string,
+}) as Ref
+const rules = {
+	form: {
+		email_address: { required, email },
+	}
+}
+const validate = useVuelidate(rules, form)
+
+function submit(): void {
+	this.validate.$validate()
+}
+</script>
